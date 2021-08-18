b960ef73bffbd133e6fa54d86737da7e09ce4bda
g1pd78 <g1pd78@gmail.com>
2021-08-18 22:35:18 +0500

    Добавьте файлы проекта.

---
+++ stack/stack.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include "stack.h"
+
+int main()
+{
+	Stack stack;
+	stack.push(5);
+	stack.push(1);
+	std::cout << stack.pop() << '\n';
+	std::cout << stack.peek() << '\n';
+	std::cout << stack.pop() << '\n';
+	std::cout << stack.pop() << '\n';//error test
+	std::cout << "isEmpty : " << stack.isEmpty() << '\n';
+	stack.push(11);
+	std::cout << "Size = " << stack.getSize() << '\n';
+	std::cout << "isEmpty : " << stack.isEmpty() << '\n';
+	stack.push(1);
+	std::cout << "Size = " << stack.getSize() << '\n';
+}
---
+++ stack/stack.h
@@ -0,0 +1,63 @@
+#pragma once
+#define STACK_SIZE 1000
+
+class Stack {
+	int* arr;
+	int size;
+	int capacity;
+public:
+	Stack() ;
+	~Stack() ;
+
+	int getSize() { return size; };
+	bool isEmpty();
+	int peek();
+	int pop();
+	void push(int );
+};
+
+Stack::Stack(){
+	arr = new int[STACK_SIZE];
+	size = 0;
+	capacity = STACK_SIZE;
+}
+
+bool Stack::isEmpty() {
+	if (size == 0)return true;
+	else return false;
+}
+
+int Stack::peek() {
+	int index = size - 1;
+	if (size > 0) {
+		return arr[index];
+	}
+	else {
+		std::cout << "Stack is empty!\n";
+		return 0;
+	}
+}
+
+int Stack::pop() {
+	int index = size - 1;
+	if (size > 0) {
+		size--;
+		return arr[index];
+	}
+	else {
+		std::cout << "Stack is empty!\n";
+		return 0;
+	}
+}
+
+void Stack::push(int inputNum) {
+	if (size >= capacity) {
+		std::cout << "Out of range!";
+		return;
+	}
+	arr[size++] = inputNum;
+}
+
+Stack::~Stack() {
+	delete[] arr;
+}
